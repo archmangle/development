d5c62462dbcaa10c3a350131387208014ad9f6ba
archmangle <tgw@furiantech.com>
2013-05-16 18:48:12 +0000

    :q!

--- ruby/azag/atomizer.rb
+++ ruby/azag/atomizer.rb
@@ -12,9 +12,29 @@ gamescape_width = 100
 gamescape_length = 100
 gamescape_max_height = 10
 
+#key arrays
+zones = []
+atoms = []
+wind_vectors = [n,e,w,s,ne,nw,se,sw]
+
+def generate_zone
+	#atoms are classed into zones
+	#and inherit the key (in the game context) characteristics
+	#of the zone they belong to
+	#so we need to generate the Zones, then place 
+	#atoms in zones, then extract and apply the attributes of the atoms
+	#by way of querying the relevant zone's attributes
+	#process:
+	#1. generate the center: number between 5 and 95 contigous across 1 to 100
+	#hlength = 10,breadth = 10,height rand(10),temperature = rand(-5,40) ,humidity = rand(0.1,0.9),wind_speed = rand(10),wind_direction(wind_vectors),
+	#barometric_pressure = rand(5),vulnerability rand(10),opacity = rand(10),penetrability = rand(10),
+	#center = allocate_center(), atom_id = allocate_id(),tile_id = zone_id(),date = date() 
+end
+
 def generate_center
 	#generate the center of the atom 
 	#ensuring no two atoms occupy the same space :-)
+	#1. obtain a number betweenn
 end
 
 def generate_atmosphere
@@ -38,16 +58,25 @@ end
 def generate_vulnerability
 	#what is "vulnerability" in the context
 	#of the game plane
+	#it is a value between 1 and 10
+	#it is a measure of weapons to have an effect 
+	#on forces gathered there
 end
 
 def generate_penetrability
 	#what is penetrability in the context 
 	#of the game plane ?
+	#it is a value between 1 and 10
+	#it is a measure of the ability of weapons
+	#to reach there 
 end
 
 def generate_opacity
 	#what is opacity in the
 	#context of the game plane ?
+	#it is a value between  1 and 10
+	#it is a measure of the opposing side to percieve
+	#the presence of forces there
 end
 
 def allocate_tile
