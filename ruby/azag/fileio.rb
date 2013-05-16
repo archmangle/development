a3ebaa73c8866ffd20e899e52b40d2c9b7e9c739
archmangle <tgw@furiantech.com>
2013-05-16 17:46:41 +0000

    added more sensible file io

--- ruby/azag/fileio.rb
+++ ruby/azag/fileio.rb
@@ -8,14 +8,39 @@
 #3. format as a sql statement
 #4. insert into GameDB.atoms
 #1. obtain atom data from csv file
-atomFile = File.new("atoms.dat","r")
-if atomFile
-        content = atomFile.sysread(20)
-        puts content
-else
-        puts "Unable to open file"
+#atomFile = File.new("atoms.dat","r")
+#if atomFile
+#        content = atomFile.sysread(200)
+#        puts content
+#else
+#        puts "Unable to open file"
+#end
+#atomFile.close
+
+#name of the data file containing 
+#atoms data
+debug = 2
+atomFile = "atoms.dat"
+atom_data = [] 
+
+#read in the file and print out the contents
+File.open(atomFile, "r").each_line do |line|
+  line = line.strip
+	if debug == 1
+		puts "D: #{line}"
+	end
+	#push onto array, after converting the line to string
+	atom_data.push line.to_s
 end
-atomFile.close
+
+if debug == 2
+	#read out the lines from the array
+	#because we have no faith
+	atom_data.each do |l| 
+		puts "L: #{l}"
+	end
+end
+
 #2. Perform any necessary formatting and calculations of the values
 #3. format as a sql statement
 #4. insert into GameDB.atoms
