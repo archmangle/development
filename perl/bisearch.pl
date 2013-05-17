060e13fb57e537cca72eeb64138e2fa43d772c16
archmangle <tgw@furiantech.com>
2013-05-17 18:58:43 +0000

    a further elabouration on this algorithm which seems imperfect at first

--- perl/bisearch.pl
+++ perl/bisearch.pl
@@ -1,30 +1,26 @@
 #!/usr/bin/perl
 #An exploration of the binary search algorithm
 #tgw 17052013
+
 $word = "psi";
 my @array = (alpha, beta, gamma, delta, epsilon, zeta, eta, theta, omega, phi, pi, psi, omicron);
-$index = binary_search(\@array, $word);
-print "D: $index -> $word \n";
-my $cnt = 0;
-foreach my $ind (@array){
-#	print "D: INDEX: $cnt WORD: $ind \n";
-	$cnt++;
+my @werds = (alpha, beta, gamma, delta, epsilon, zeta, eta, theta, omega, phi, pi, psi, omicron);
+
+foreach my $word (@werds){
+ $index = binary_search(\@array,$word);
+ print "D: INDEX: $index WORD: $word \n";
 }
+
 sub binary_search {
-	my ($array, $word) = @_;
-	my ($low, $high) = (0, @$array - 1);
-	my $monkey = @$array-1;
-	#debug point:
-	#print "D: High: $high , \@\$array - one is : "."$monkey"."\n";
+	my ($array,$word) = @_;
+	my ($low,$high) = (0, (@$array -1));
+	print "(initialising) HIGH: $high LOW: $low \n";
 	while( $low <= $high ){
-		print "D0: $low ~ $high WORD: $word\n";
-		my $try = int(($low+$high)/2);
+		print "D: $low : $high \n";
+		$try = int(($low+$high)/2);
 		$low = $try+1, next if $array->[$try] lt $word;
-		$test = $array->[$try];
-		print "D1: $test ~ $word \n";
-		$high = $try+1, next if $array->[$try] gt $word;
-		$test = $array->[$try];
-		print "D2: $test ~ $word\n";
+		$high = $try-1, next if $array->[$try] gt $word;
+		print "TRY: $try \n";
 		return $try;
 	}
 	return;
